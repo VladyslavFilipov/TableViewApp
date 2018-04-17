6ca37312ea6cbfe16c7fdd113a7a115a51982b0a
Vladyslav Filipov <38040492+VladyslavFilipov@users.noreply.github.com>
2018-04-17 11:41:09 +0300

    delete all unneeded stuff

--- TableViewApp/TableViewApp/DescriptionViewController.swift
+++ TableViewApp/TableViewApp/DescriptionViewController.swift
@@ -11,15 +11,12 @@ import UIKit
 class DescriptionViewController: UIViewController {
 
     @IBOutlet weak var descriptionViewControllerLabel: UILabel!
-    @IBOutlet weak var descriptionViewControllerScrollView: UIScrollView!
     @IBOutlet weak var descriptionViewControllerNavigationBar: UINavigationItem!
-    @IBOutlet weak var descriptionViewControllerScrollViewUIView: UIView!
     
     var cellEntity = DataStructureModel(title: "")
     
     override func viewWillAppear(_ animated: Bool) {
         descriptionViewControllerNavigationBar.title = cellEntity.getTitle()
         descriptionViewControllerLabel.text = cellEntity.getDescr()
-        descriptionViewControllerScrollView.contentLayoutGuide.bottomAnchor.constraint(equalTo: descriptionViewControllerScrollViewUIView.bottomAnchor).isActive = true
     }
 }
